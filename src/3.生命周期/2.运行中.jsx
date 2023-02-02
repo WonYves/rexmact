dca752986bdb52aba88c982af72fe81f1932e8ca
Wiess <1053137796@qq.com>
2023-02-02 10:45:32 +0800

    bts

--- src/3.生命周期/2.运行中.jsx
+++ src/3.生命周期/2.运行中.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  state = {
+    myname : 'kerwin'
+  }
   render() {
     return (
-      <div>App</div>
+      <div>
+        <button onClick={() => {
+          this.setState({
+            myname: 'xiaoming'
+          })
+        }}>click</button>
+        <div id='myname'>{this.state.myname}</div>
+      </div>
     )
+  }
+
+  UNSAFE_componentWillUpdate(){
+    console.log(document.getElementById('myname').innerHTML)
+  }
+  componentDidUpdate(){
+    console.log(document.getElementById('myname').innerHTML)
   }
 }
