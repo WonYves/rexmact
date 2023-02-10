491635433ca1f950eb4668f41646bd734ef53720
Wiess <1053137796@qq.com>
2023-02-10 17:55:23 +0800

    1

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+// 设置代理的JS文件
+module.exports = function(app){
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin:true,
+    })
+  )
+}
