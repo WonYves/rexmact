3557e2136d844348a7203a79825927ef9782ae15
Wiess <1053137796@qq.com>
2023-02-15 15:58:34 +0800

    ball shit

--- src/8.immutable/2.map.jsx
+++ src/8.immutable/2.map.jsx
@@ -5,17 +5,12 @@ import { fromJs } from 'immutable'
 const ImMap = () => {
 
   const [data] = useState({name: 'kerwin',city: ['beijin', 'dalian', 'shanghai']})
-  // setIn(['name'], 'kerwin')
-  // undateIn(['city'], (list) => {
-  //   list.splice(index,1)
-  // })
   return (
     <Fragment>
       <div>
         <ul>
-          {data.city.map((item, index) => {
-            return <li key={index}>{item}</li>
-          })}
+          {data.city.map((item, index) => <li key={index}>{item}</li>
+          )}
         </ul>
       </div>
     </Fragment>
